aa5cb4d6ae14014929e1316c12a39e3e6ef65e8c
Memich69 <michalmesika2@gmail.com>
2024-07-29 19:58:30 +0300

    first commit

---
+++ DisplayDate-master/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DisplayDate
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            int miri = 5;
+            InitializeComponent();
+            int y = 6;
+            DateTime currentTime = DateTime.Now;
+            string formattedTime = currentTime.ToString("HH:mm:ss");
+            string formattedDate = currentTime.ToString("dd.MM.yyyy");
+
+            myLabel.Content = "Current time: " + formattedTime + ", Current date: " + formattedDate;
+            myLabel.Content = currentTime;
+        }
+    }
+}
